1f42c81ec58ab333bcb753da92ef351f8c2b34af
Ubuntu <ubuntu@large-react-diary.1j4iphubq4refk1fk52gkli2ch.bx.internal.cloudapp.net>
2020-03-25 19:47:19 +0000

    added a menu bar

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -11,18 +11,20 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
+import MenuBar from './menu-bar.jsx';
+
 const useStyles = makeStyles({
     card: {
-      maxWidth: 600,
-      margin: 'auto',
-      marginTop: '10px'
+        maxWidth: 600,
+        margin: 'auto',
+        marginTop: '10px'
     },
     title: {
-      padding: '5px',
-      color: 'blue'
+        padding: '5px',
+        color: 'blue'
     },
     media: {
-      minHeight: 330
+        minHeight: 330
     }
 });
 
@@ -42,21 +44,24 @@ export default function Home() {
     if (toJournal) {
         return (<Redirect to='/journal' />);
     } else return (
-        <React.Fragment>
-            <CssBaseline />
-            <Container maxWidth="lg">
-                <Typography component="div" style={{ backgroundColor: '#ffe6e6', height: '100vh' }}>
-                    <Typography type="headline" component="h2" className=
-                    {classes.title}>
-                        Home Page
+        <div>
+            <MenuBar></MenuBar>
+            <React.Fragment>
+                <CssBaseline />
+                <Container maxWidth="lg">
+                    <Typography component="div" style={{ backgroundColor: '#ffe6e6', height: '100vh' }}>
+                        <Typography type="headline" component="h2" className=
+                        {classes.title}>
+                            Home Page
+                        </Typography>
+                        <div>create journal is {toJournal}</div>
+                        <Typography type="body1" component="p">
+                            Welcome to the diary app
+                        </Typography>
+                        <Button variant="contained" color="primary" onClick={onCreateButtonClicked}>Create Journal</Button>
                     </Typography>
-                    <div>create journal is {toJournal}</div>
-                    <Typography type="body1" component="p">
-                        Welcome to the diary app
-                    </Typography>
-                    <Button variant="contained" color="primary" onClick={onCreateButtonClicked}>Create Journal</Button>
-                </Typography>
-            </Container>
-        </React.Fragment>
+                </Container>
+            </React.Fragment>
+        </div>
     )
 }
