a2aed779116c6434613add0e24ecbc4513ba2cb5
Ubuntu <ubuntu@large-react-diary.1j4iphubq4refk1fk52gkli2ch.bx.internal.cloudapp.net>
2020-03-26 19:44:50 +0000

    styled the front page

--- client/App.jsx
+++ client/App.jsx
@@ -3,13 +3,30 @@ import MainRouter from './MainRouter.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 import MenuBar from './core/menu-bar.jsx';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
+const theme = createMuiTheme({
+    palette: {
+        primary: purple,
+        secondary: green,
+    },
+    status: {
+        danger: 'orange',
+    },
+});
 
 const App = () => (
-    <BrowserRouter>
-        <MenuBar></MenuBar>
-        <MainRouter/>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+            <MenuBar></MenuBar>
+            <MainRouter/>
+        </BrowserRouter>
+    </ThemeProvider>
 )
 
 export default hot(module)(App)
--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -1,28 +1,22 @@
 import React, {useState} from 'react'
 import {Redirect} from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import { create } from './../journal/api';
 import { Journal } from './../journal/journal.model';
-
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
-    card: {
-        maxWidth: 600,
-        margin: 'auto',
-        marginTop: '10px'
-    },
     title: {
-        padding: '5px',
-        color: 'blue'
+        fontSize: '80pt',
     },
-    media: {
-        minHeight: 330
+    subtitle: {
+
+    },
+    container: {
+        textAlign: 'center',
     }
 });
 
@@ -43,19 +37,16 @@ export default function Home() {
         return (<Redirect to='/journal' />);
     } else return (
         <React.Fragment>
-            <CssBaseline />
-            <Container maxWidth="lg">
-                <Typography component="div" style={{ backgroundColor: '#ffe6e6', height: '100vh' }}>
-                    <Typography type="headline" component="h2" className=
-                    {classes.title}>
-                        Home Page
-                    </Typography>
-                    <div>create journal is {toJournal}</div>
-                    <Typography type="body1" component="p">
-                        Welcome to the diary app
-                    </Typography>
-                    <Button variant="contained" color="primary" onClick={onCreateButtonClicked}>Create Journal</Button>
+            <Container className={classes.container}>
+                <Typography type="headline" component="h1" className={classes.title}>
+                    Create a Daily Journal
                 </Typography>
+                <Box  mb={10}>
+                    <Typography type="headline" component="h5" className={classes.subtitle}>
+                        Note: Your journal will be viewable and editable to anyone who has the link!
+                    </Typography>
+                </Box>
+                <Button variant="contained" onClick={onCreateButtonClicked}>CREATE A NEW JOURNAL</Button>
             </Container>
         </React.Fragment>
     )
