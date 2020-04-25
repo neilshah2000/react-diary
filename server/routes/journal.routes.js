a12d03779a9eeee07ec670ad38a3e8b38765340c
Ubuntu <ubuntu@ip-172-31-40-203.ec2.internal>
2020-04-25 12:40:41 +0000

    removed trash from router

--- server/routes/journal.routes.js
+++ server/routes/journal.routes.js
@@ -7,24 +7,6 @@ const router = express.Router();
 router.route('/prompt')
   .get(promptCtrl.list)
 
-router.route('test').get((req, res) => {
-    return res.json({test: 'test'});
-});
-
-router.route('').get((req, res) => {
-    return res.json({test: ''});
-});
-
-router.route('/').get((req, res) => {
-    return res.json({test: '/'});
-});
-
-router.route('/test').get((req, res) => {
-    debugger;
-    console.log('test');
-    return res.json({test: '/test'});
-});
-
 router.route('/journal')
   .get(journalCtrl.list)
   .post(journalCtrl.create);
